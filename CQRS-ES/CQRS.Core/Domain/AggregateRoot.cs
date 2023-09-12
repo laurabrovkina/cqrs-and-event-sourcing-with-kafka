a6704dff640c7b775ba762bd91630fb62c28c531
Larisa <laurabrovkina@gmail.com>
2023-09-13 10:37:26 +1200

    Create AggregateRoot

---
+++ CQRS-ES/CQRS.Core/Domain/AggregateRoot.cs
@@ -0,0 +1,58 @@
+﻿using CQRS.Core.Events;
+
+namespace CQRS.Core.Domain;
+
+public abstract class AggregateRoot
+{
+    protected Guid _id;
+    private readonly List<BaseEvent> _changes = new();
+
+    public Guid Id
+    {
+        get {  return _id;  }
+    }
+
+    public int Version { get; set; } = -1;
+
+    public IEnumerable<BaseEvent> GetUncommitedChanges()
+    {
+        return _changes;
+    }
+
+    public void MarkChangesAsCommitted()
+    {
+        _changes.Clear();
+    }
+
+    private void ApplyChanges(BaseEvent @event, bool isNew)
+    {
+        // this will give us a type of concrete aggregate when we inherit this aggregare root
+        // we are using a reflection, apply method can be only used in concrete class
+        var method = this.GetType().GetMethod("Apply", new Type[] { @event.GetType() });
+
+        if(method == null)
+        {
+            throw new ArgumentNullException(nameof(@event), $"The Apply method was not found in the aggregate for {@event.GetType().Name}");
+        }
+
+        method.Invoke(this, new object[] { @event });
+
+        if(isNew)
+        {
+            _changes.Add(@event);
+        }
+    }
+
+    protected void RaiseEvent(BaseEvent @event)
+    {
+        ApplyChanges(@event, true);
+    }
+
+    public void ReplayEvents(IEnumerable<BaseEvent> events)
+    {
+        foreach (var @event in events)
+        {
+            ApplyChanges(@event, false);
+        }
+    }
+}
