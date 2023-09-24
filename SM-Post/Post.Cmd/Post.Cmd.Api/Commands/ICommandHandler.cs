e6d5ec911eca17a49ae4f59371c2be922a98a48a
Larisa <laurabrovkina@gmail.com>
2023-09-25 12:09:24 +1300

    Command handling

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/ICommandHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Post.Cmd.Api.Commands;
+
+public interface ICommandHandler
+{
+    Task HandlerAsync(NewPostCommand command);
+    Task HandlerAsync(EditMessageCommand command);
+    Task HandlerAsync(LikePostCommand command);
+    Task HandlerAsync(AddCommentCommand command);
+    Task HandlerAsync(EditCommentCommand command);
+    Task HandlerAsync(RemoveCommentCommand command);
+    Task HandlerAsync(DeletePostCommand command);
+}
--- SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,6 +1,11 @@
 using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
+using Post.Cmd.Api.Commands;
+using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Dispatchers;
+using Post.Cmd.Infrastructure.Hadlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
 
@@ -11,6 +16,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+// register command handler methods
+var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispatcher = new CommandDispatcher();
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<AddCommentCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<EditCommentCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<RemoveCommentCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandlerAsync);
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
