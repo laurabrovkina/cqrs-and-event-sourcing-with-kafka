cd8f2fe89a8fc7c523ebc950752eef1f8a1e989f
Larisa <laurabrovkina@gmail.com>
2023-09-25 15:07:25 +1300

    Remove ConfigureAwait(false)

--- SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Repositories/EventStoreRepository.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Repositories/EventStoreRepository.cs
@@ -1,4 +1,4 @@
-﻿using CQRS.Core.Domain;
+using CQRS.Core.Domain;
 using CQRS.Core.Events;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
@@ -17,14 +17,16 @@ public class EventStoreRepository : IEventStoreRepository
 
         _eventStoreCollection = mongoDatabase.GetCollection<EventModel>(config.Value.Collection);
     }
-
+    //Update: ASP.NET Core does not have a SynchronizationContext.If you are on ASP.NET Core,
+    // it does not matter whether you use ConfigureAwait(false) or not.
+    //Ref : https://stackoverflow.com/questions/13489065/best-practice-to-call-configureawait-for-all-server-side-code
     public async Task<List<EventModel>> FindByAggregateId(Guid aggregateId)
     {
-        return await _eventStoreCollection.Find(x => x.AggregateIdentifier == aggregateId).ToListAsync().ConfigureAwait(false);
+        return await _eventStoreCollection.Find(x => x.AggregateIdentifier == aggregateId).ToListAsync();
     }
 
     public async Task SaveAsync(EventModel @event)
     {
-        await _eventStoreCollection.InsertOneAsync(@event).ConfigureAwait(false);
+        await _eventStoreCollection.InsertOneAsync(@event);
     }
 }
