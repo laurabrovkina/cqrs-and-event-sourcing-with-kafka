b81799f8222218aba4f34db60cc1d2f8964fc6da
Larisa <laurabrovkina@gmail.com>
2023-11-14 16:50:28 +1300

    Create ConsumerHostedService

---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Consumers/ConsumerHostedService.cs
@@ -0,0 +1,41 @@
+﻿using CQRS.Core.Consumers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Post.Query.Infrastructure.Consumers;
+
+public class ConsumerHostedService : IHostedService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<ConsumerHostedService> _logger;
+
+    public ConsumerHostedService(IServiceProvider serviceProvider,
+        ILogger<ConsumerHostedService> logger)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Event consumer service running...");
+
+        using (IServiceScope scope = _serviceProvider.CreateScope())
+        {
+            var eventConsumer = scope.ServiceProvider.GetRequiredService<IEventConsumer>();
+            var topic = Environment.GetEnvironmentVariable("KAFKA_TOPIC");
+
+            Task.Run(() => eventConsumer.Consume(topic), cancellationToken);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Event consumer service stopped.");
+
+        return Task.CompletedTask;
+    }
+}
